9f33d47a0a0565603b3dc1f57ef99485cd1051dd
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-25 16:35:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,3 +3,15 @@ def sort_array_asc(ints)
     a <=> b
   end
 end
+
+def sort_array_desc(ints)
+  ints.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
